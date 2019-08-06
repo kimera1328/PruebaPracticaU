462145106fb9f15c9b866ca1733cb740b95689ea
LeonardoRubio <53800934+kimera1328@users.noreply.github.com>
2019-08-05 19:36:57 -0500

    PruebaPractica

---
+++ index.php
@@ -0,0 +1,7 @@
+<?php 
+require_once('conexion/conexion.php');
+require_once('Controlador/controladorPrincipal.php');
+require_once('Modelo/generalModelo.php');
+
+$plantilla = principalControlador::ctrPrincipal();
+?>
